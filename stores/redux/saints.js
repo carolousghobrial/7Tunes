c5c2d568d1981b318ab708795dee74acb5c9c397
carolous.ghobrial@gmail.com <carolous.ghobrial@gmail.com>
2023-04-12 15:18:46 -0500

    More Changes

--- stores/redux/saints.js
+++ stores/redux/saints.js
@@ -6,7 +6,6 @@ import { CurrentSeason } from "../../models/currentSeason";
 import moment from "moment";
 
 const saintsSlice = createSlice({
-  
   name: "saints",
   initialState: {
     ARCHANGEL_MICHAEL: {
