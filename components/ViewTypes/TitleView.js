928e2403cf18fe3ffba1713911d1de6e03e5d56a
Carolous Ghobriall <carolous.ghobrial@gmail.com>
2023-12-21 00:41:13 -0600

    dasdad

--- components/ViewTypes/TitleView.js
+++ components/ViewTypes/TitleView.js
@@ -12,7 +12,6 @@ import {
 import bookPaths from "../../helpers/bookPathsHelpers";
 
 import images from "../../helpers/imageHelpers";
-import { shareAsync } from "expo-sharing";
 import { Entypo } from "@expo/vector-icons";
 
 import {
